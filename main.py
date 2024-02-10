8f971d28c586ebfefb7c5cfe00ffcf4cd1cc367f
Yesid rodriguez Escorcia <megasoldier15x@hotmail.com>
2024-02-10 16:51:59 -0500

    Programa terminado

--- main.py
+++ main.py
@@ -1,1 +1,22 @@
+"""Mostrar los numeros primos con una distancia de 2(gemelos), 4(primos) y 6(sexy)"""
 from primos import NumPrimos
+from random import randint
+
+def mostrar_info(lista, nombre):
+    print(f"{nombre} -> {lista}")
+    
+def extraer_numeros(lista, rango):
+    extraidos = []
+    for i in range(len(lista) - 1):
+        for j in range(i + 1, len(lista)):
+            if lista[i] + rango == lista[j]:
+                extraidos.append(f"{lista[i]} : {lista[j]}")
+    return extraidos
+        
+primos = NumPrimos(randint(2, 100))
+num_primos = primos.generar_num_primos()
+print(num_primos)
+ 
+mostrar_info(extraer_numeros(num_primos, 2), "Gemelos")
+mostrar_info(extraer_numeros(num_primos, 4), "Primos")
+mostrar_info(extraer_numeros(num_primos, 6), "Sexy")
--- primos.py
+++ primos.py
@@ -1,4 +1,27 @@
 #Clase que maneja numeros primos mediante la criba de Eratostenes
 
 class NumPrimos:
-    pass
+    def __init__(self, num_limite):
+        self.__num_limite = num_limite
+        self.__num_primos = []
+        
+    def generar_num_primos(self)->list:
+        """Genera numeros primos usando la criba de Eratostenes. Devuelve una lista de enteros"""
+        #Se empieza asumiendo que todos los numeros son primos. Los indices de la lista marcan 
+        # los numeros. El 1 y el 0 no son primos
+        primos = [True for i in range(self.__num_limite + 1)]
+        primos[0], primos[1] = False, False
+        
+        #Se busca marcar los numeros que sean multiplos (i * j) de numero base (i), esos no serian primos. 
+        # La busqueda termina hasta el que el multiplo no sea mayor al cuadrado del numero base.
+        i = 2
+        while i * i <= self.__num_limite:
+            j = i
+            while j * i <= self.__num_limite:
+                primos[i * j] = False
+                j += 1
+            i += 1
+        
+        self.__num_primos = [i for i in range(2, self.__num_limite + 1) if primos[i] == True]
+        
+        return self.__num_primos
